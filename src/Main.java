5e150291e3294f298e28c2b2a7b130b8d41d88e6
cdxgh <khanarshu0786@gmail.com>
2025-03-10 12:22:04 +0530

    new file

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Uncommented part
-        String result = "30"; // String containing a number
-        Integer res = Integer.parseInt(result); // Convert string to integer
-        System.out.println(res + 30); // Output: 60
+        String result = "Arshu khan"; // String containing a number
+        System.out.println(result.toUpperCase());
     }
 }
