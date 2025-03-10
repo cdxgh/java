f5731a49d1180f08b133bb171af572f4806e7f87
cdxgh <khanarshu0786@gmail.com>
2025-03-10 10:27:10 +0530

    new

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] arr){
+        System.out.println("Hello Hammad");
+        System.out.println(10+5);
+        System.out.println(94904);
+
+    }
+}
