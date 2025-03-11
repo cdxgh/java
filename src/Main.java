d79013020728212a8ccb37044285ba25225154a1
cdxgh <khanarshu0786@gmail.com>
2025-03-11 10:35:30 +0530

    new file

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         // Uncommented part
-        String result = "Arshu khan"; // String containing a number
-        System.out.println(result.toUpperCase());
+        char a = 'p';
+
+        System.out.println(a);
+
     }
 }
