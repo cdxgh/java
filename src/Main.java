aaace9be1ac2c18d0438488cc58eb4147e637b5f
cdxgh <khanarshu0786@gmail.com>
2025-03-11 11:13:19 +0530

    new file

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        // Uncommented part
-        char a = 'p';
+        int[] arr = new int[10]; // Declare an array of size 10
 
-        System.out.println(a);
+        // Assign 5 to each element
+        for (int i = 0; i < 10; i++) {
+            arr[i] = 5;
+        }
 
+        // Print the array elements
+        for (int i = 0; i < 10; i++) {
+            System.out.println(arr[i]); // This will print '5' ten times
+        }
     }
 }
