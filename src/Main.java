01e8984843c9005e0864d2424e9398a01de1c214
cdxgh <khanarshu0786@gmail.com>
2025-03-16 09:34:08 +0530

    new file

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,10 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[10]; // Declare an array of size 10
-
-        // Assign 5 to each element
-        for (int i = 0; i < 10; i++) {
-            arr[i] = 5;
-        }
-
-        // Print the array elements
-        for (int i = 0; i < 10; i++) {
-            System.out.println(arr[i]); // This will print '5' ten times
-        }
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(5);
+        arr.add(6);
+        System.out.println(arr);
     }
 }
