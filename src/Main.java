f380015e08ad2dde93456f8859ba78ae48697e14
cdxgh <khanarshu0786@gmail.com>
2025-03-27 11:37:39 +0530

    new file

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,15 @@ public class Main {
 //        for(int i =0; i<10; i++){
 //            System.out.println("i+1");
 //        }
-        int i =0;
-        while (i<10){
+//        int i =0;
+//        while (i<10){
+//            System.out.println(i);
+//            i = i+1;
+        int i=12;
+        do{
             System.out.println(i);
-            i = i+1;
+            i=i+1;
 
-        }
+        } while(i< 10);
     }
 }
